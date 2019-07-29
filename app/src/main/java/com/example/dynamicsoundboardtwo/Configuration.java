c965d908568b748431f28165da99cba28cc71ebb
MattBrooks95 <xmatthew3x@gmail.com>
2019-07-29 17:59:56 +0900

    having a real hard time trying to figure out how to get from the URI the user selected to being able to parse that directory and access the json configuration file, it seems like there's not a clear way to go from an android uri to a file object and that makes me sad. I'm sure I'm just missing something.

--- app/src/main/java/com/example/dynamicsoundboardtwo/Configuration.java
+++ app/src/main/java/com/example/dynamicsoundboardtwo/Configuration.java
@@ -1,4 +1,27 @@
 package com.example.dynamicsoundboardtwo;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
 public class Configuration {
+    private static final String TAG = "Configuration";
+    private static final String configuration_file_name = "image_to_sound_mapping.json";
+    private String configuration_file_content;
+    Configuration(Uri folder_uri){
+        Uri.Builder compounded_uri_builder = folder_uri.buildUpon().appendPath(configuration_file_name);
+        Uri compounded_uri   = compounded_uri_builder.build();
+        initialize_configuration_object_from_uri(compounded_uri);
+    }
+
+    private void initialize_configuration_object_from_uri(Uri file_uri){
+        String uri_as_string = file_uri.getPath();
+        Log.d(TAG,"appended uri:" + uri_as_string);
+    }
 }
--- app/src/main/java/com/example/dynamicsoundboardtwo/DynamicSoundBoard.java
+++ app/src/main/java/com/example/dynamicsoundboardtwo/DynamicSoundBoard.java
@@ -2,11 +2,12 @@ package com.example.dynamicsoundboardtwo;
 
 import android.app.Activity;
 import android.content.Intent;
+
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+
 import android.widget.TextView;
 
 public class DynamicSoundBoard extends AppCompatActivity {
@@ -15,42 +16,46 @@ public class DynamicSoundBoard extends AppCompatActivity {
 
     public static final int GET_FOLDER_REQUEST_CODE = 1776;
 
+    public Uri user_selected_media_directory_uri;
+
     protected Configuration configuration    = null;
     protected DisplayManager display_manager = null;
-
-    public Uri user_selected_media_directory;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setup_root_media_directory();
+        setup_root_media_directory_uri();
     }
 
     private void setup(){
-        setup_configuration();
-        setup_display();
+        test_print();
+
+        setup_configuration(user_selected_media_directory_uri);
+        setup_display(user_selected_media_directory_uri);
+    }
+
+    private void test_print(){
         TextView greeting = findViewById(R.id.greeting);
-        greeting.append(user_selected_media_directory.toString());
+        greeting.append(user_selected_media_directory_uri.toString());
     }
 
-    private void setup_configuration(){
+    private void setup_configuration(Uri folder_uri){
         Log.d(TAG,"setup_configuration()");
+        configuration = new Configuration(folder_uri);
     }
 
-    private void setup_display(){
+    private void setup_display(Uri folder_uri){
         Log.d(TAG,"setup_display()");
     }
 
-    private void setup_root_media_directory(){
+    private void setup_root_media_directory_uri(){
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
         startActivityForResult(intent, GET_FOLDER_REQUEST_CODE);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent resultData){
-
         // The ACTION_OPEN_DOCUMENT intent was sent with the request code
         // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
         // response to some other intent, and the code below shouldn't run at all.
@@ -60,9 +65,10 @@ public class DynamicSoundBoard extends AppCompatActivity {
             // Instead, a URI to that document will be contained in the return intent
             // provided to this method as a parameter.
             // Pull that URI using resultData.getData().
-            if (resultData != null) {
-                user_selected_media_directory = resultData.getData();
-                Log.i(TAG, "Uri: " + user_selected_media_directory.toString());
+            if (resultData != null){
+                user_selected_media_directory_uri = resultData.getData();
+
+                Log.d(TAG, "Uri: " + user_selected_media_directory_uri.toString());
                 setup();
             }
         }
